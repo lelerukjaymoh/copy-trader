95860b1b72c1dbb9a9483d0ead3ec3207ab37086
Jay <lelerukjaymoh@gmail.com>
2024-01-05 00:39:58 +0300

    feat: get tradde direction and tokens involved in trade
    - manually splits the call data to get the position of the in and out tokens from the data string

--- index.ts
+++ index.ts
@@ -1,45 +1,55 @@
+import { wallets } from "./src/constants";
 import { provider } from "./src/utils";
 
 const main = async () => {
     try {
-        console.log("chain ", await provider.getNetwork())
+        const hash = "0xf5b18590364560657e48453d77be49ceb6a2ad411b120667b025ac04152f8bbb"
 
-        const hash = "0xa34ec2da4136a9107cdd71533070a5300d3590d6c7c59e168980fde2e64779b4"
+        console.log("wallets to follow: ", wallets)
 
         // provider.on("pending", async (hash: string) => {
-        const txData = await provider.getTransaction(hash)
-
-        console.log("txData", txData)
+        const txData = await provider.getTransaction(hash);
 
         if (txData) {
-            const { to, from, data } = txData!
+            const { from, data } = txData;
 
-            await provider
-                .send("debug_traceCall", [
-                    { to, from, data },
-                    "0x1f5416e37a1df1970221038411bb92b94d714123e37d63e60093ee2f16f7192f",
-                    {
-                        tracer: "prestateTracer",
-                        disableStorage: false,
-                        tracerConfig: { diffMode: true },
-                    },
-                ])
-                .then(async (trace: any) => {
-                    for (const step of Object.keys(trace.post)) {
-                        if (trace.post[step].storage) {
-                            console.log("step ", step, trace.post[step].storage)
-                        }
-                        // console.log("step ", step, trace.post[step])
-                    }
-                    console.log("\n\nprestateTracer ", hash, trace)
-                }).catch((error: any) => {
-                    console.log("\n\nprestateTracer error", hash, error)
-                })
+            // filter only transactions executed by the wallets we are following
+            if (from && !wallets.hasOwnProperty(from.toLowerCase())) return;
+
+            const methodId = data.slice(0, 10).toLowerCase();
+
+            // Get the trade direction and token
+            if (methodId == "0x0162e2d0") {
+                console.log("BUY TRADE: ", hash, txData);
+
+                const tokenOutWithZeros = data.substring(data.length - 64);
+                const tokenInWithZeros = data.substring(
+                    data.length - 128,
+                    data.length - 64
+                );
+                const tokenIn = "0x" + tokenInWithZeros.slice(24);
+                const tokenOut = "0x" + tokenOutWithZeros.slice(24);
+
+                console.log("tokenIn: ", tokenIn);
+                console.log("tokenOut: ", tokenOut);
+
+            } else if (methodId == "0x75713a08") {
+                console.log("SELL TRADE: ", hash, txData);
+
+                const tokenInWithZeros = data.slice(10, 74);
+                const tokenOutWithZeros = data.slice(74, 138);
+                const tokenIn = "0x" + tokenInWithZeros.slice(24);
+                const tokenOut = "0x" + tokenOutWithZeros.slice(24);
+
+
+                console.log("tokenIn: ", tokenIn);
+                console.log("tokenOut: ", tokenOut);
+            }
         }
-        // })
+        // });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-main()
+main();
